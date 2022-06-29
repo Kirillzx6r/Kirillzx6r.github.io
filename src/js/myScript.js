b6e66ce1e7c7ee5cb8eeecf23d7292adee474289
Kirillzx6r <103196561+Kirillzx6r@users.noreply.github.com>
2022-06-30 00:46:15 +0700

    Add files via upload

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -4,3 +4,6 @@ $(document).ready(function(){
       let valHref =$(this).attr("href");
       $('html,body').animate({scrollTop: $(valHref).offset().top - 200 + "px"})
   });
+    
+new WOW().init();
+});
