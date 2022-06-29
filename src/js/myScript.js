cf4d4b2ea3b4f93319f10d20fbc8c0bd91d68d55
Kirillzx6r <103196561+Kirillzx6r@users.noreply.github.com>
2022-06-29 17:13:16 +0700

    Add files via upload

---
+++ src/js/myScript.js
@@ -0,0 +1,6 @@
+"use strict"
+$(document).ready(function(){
+      $('a[href^="#"]').click(function(){
+      let valHref =$(this).attr("href");
+      $('html,body').animate({scrollTop: $(valHref).offset().top - 200 + "px"})
+  });
