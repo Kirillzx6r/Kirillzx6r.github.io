6e91b01a2ecda749e3db7eb7f5b8172b8cf7e031
Kirillzx6r <103196561+Kirillzx6r@users.noreply.github.com>
2022-07-11 19:11:09 +0700

    Add files via upload

--- src/php/mail.php
+++ src/php/mail.php
@@ -4,22 +4,24 @@ require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
-$name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
 $email = $_POST['user_email'];
+$phone = $_POST['user_phone'];
+$order = $_POST['user_order'];
+$radio = $_POST['Radios'];
+$checkbox = $_POST['check'];
+
 
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'example@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'password'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'kirillcbr1000rr@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'Ly2xopfn6WJuajMP34Gm'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('example@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('exampleto@p33.org');     // Кому будет уходить письмо 
+$mail->setFrom('kirillcbr1000rr@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('kozlovkirill859@gmail.com');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
@@ -28,13 +30,13 @@ $mail->addAddress('exampleto@p33.org');     // Кому будет уходит�
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name . ' оставил заявку!';
+$mail->Subject = 'Тема письма';
+$mail->Body    = '' . " Скрипт сработал! <br>" . $email. "<br>" .$phone. "<br>" .$order. "<br>" .$radio. "<br>" .$checkbox;
 $mail->AltBody = '';
 
 if(!$mail->send()) {
-    echo 'Error';
+    return false;
 } else {
-    echo 'success';
+    return true;
 }
 ?>
--- src/php/test.php
+++ src/php/test.php
@@ -1,3 +1,10 @@
 <?php
-echo " Скрипт сработал!";
+$email = $_POST['user_email'];
+$phone = $_POST['user_phone'];
+$order = $_POST['user_order'];
+$radio = $_POST['Radios'];
+$checkbox = $_POST['check']
+
+
+echo " Скрипт сработал! <br>" . $email;
 ?>
